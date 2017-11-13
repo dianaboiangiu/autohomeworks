456c25b3b2ec279027ba9fbb8f0e29b0f7e3544b
Diana Boiangiu <boiangiudiana58@gmail.com>
2017-11-13 17:33:51 +0200

    Register models to admin

--- homeworks/admin.py
+++ homeworks/admin.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from homeworks.models import *
+
 # Register your models here.
+
+admin.site.register(Course)
+admin.site.register(Homework)
+admin.site.register(Material)
+admin.site.register(Solution)
