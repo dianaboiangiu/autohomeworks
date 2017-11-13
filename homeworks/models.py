9e767954d0a2883db28065579f29daa29986c061
Diana Boiangiu <boiangiudiana58@gmail.com>
2017-11-13 17:13:35 +0200

    Add initial migration

--- homeworks/models.py
+++ homeworks/models.py
@@ -1,6 +1,39 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    created_by = models.ForeignKey(User, related_name='owner')
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    date_created = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    members = models.ManyToManyField(User, related_name='courses')
+
+    def __str__(self):
+        return 'Course {name}'.format(name=self.name)
+
+
+class Material(models.Model):
+    course = models.ForeignKey(Course)
+    created_at = models.DateTimeField(auto_now_add=True)
+    file = models.FileField()
+
+
+class Homework(models.Model):
+    course = models.ForeignKey(Course)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    test_file = models.FileField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField()
+
+
+class Solution(models.Model):
+    homework = models.ForeignKey(Homework)
+    user = models.ForeignKey(User)
+    tests_status = models.CharField(max_length=200)
+    tests_summary = models.CharField(max_length=400)
